564c923e3e07d1197be5c40f36bd2a523ee4df29
Neeraj Dalal <admin@nrjdalal.com>
2024-02-28 03:14:23 +0530

    improved way to contribute

--- bin/generate.js
+++ bin/generate.js
@@ -21,7 +21,7 @@ for (const file of files) {
 
   modifyContent = modifyContent.map((arr) => {
     const type = arr[0][0]
-    const component = `${name} ${type}`
+    const component = `${name.replace("-", " ")} ${type}`
       .split(" ")
       .map((s) => s[0].toUpperCase() + s.slice(1))
       .join(" ")
@@ -35,22 +35,22 @@ for (const file of files) {
         ? [`shadcn-x-${name}`, `cnx-${name}`]
         : [
             `shadcn-x-${name}-${type.replace(/ /g, "-")}`,
-            `cnx-${name}-${type.replace(/ /g, "-")}`
-          ]
+            `cnx-${name}-${type.replace(/ /g, "-")}`,
+          ],
     ].flat()
 
     if (type === "import") {
       return (importJson[component] = {
         prefix,
         body: [arr[1].slice(0, -1)].flat(),
-        description: `https://ui.shadcn.com/docs/components/${name}`
+        description: `https://ui.shadcn.com/docs/components/${name}`,
       })
     }
 
     return (usageJson[component] = {
       prefix,
       body: [arr[1].slice(0, -1)].flat(),
-      description: `https://ui.shadcn.com/docs/components/${name}`
+      description: `https://ui.shadcn.com/docs/components/${name}`,
     })
   })
 }
